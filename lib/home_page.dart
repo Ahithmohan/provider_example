9db123ba1640b0a46723bb20bc6beccfb43c9383
ahithmohan <ahitmohan123@gmail.com>
2024-11-05 12:07:46 +0530

    adde bottom navigation and icons

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,6 +10,44 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text("Provider Example"),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.teal,
+        shape: CircularNotchedRectangle(),
+        notchMargin: 5.0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(
+                  Icons.home,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.settings,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: const Center(
+        child: Text(
+          "data",
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.w900),
+        ),
+      ),
+    );
   }
 }
